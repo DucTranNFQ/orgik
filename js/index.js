6345e0dea87a6c63d8026bb23e6e37fa1d526131
Duc Tran <duc.tran@nfq.asia>
2022-05-16 16:41:03 +0700

    add: suport-section grid-section to-top button

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-console.log("index")
+// to top
+document.addEventListener("scroll", () => {
+    if (window.scrollY > 850) {
+        document.getElementById("to-top").style.display = "flex";
+    } else {
+        document.getElementById("to-top").style.display = "none";
+    }
+})
