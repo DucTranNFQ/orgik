d267823ed9cdf9be76efde88ebb06454c767d829
Duc Tran <duc.tran@nfq.asia>
2022-05-17 16:43:20 +0700

    add: header sticky

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 // to top
 document.addEventListener("scroll", () => {
+
+    //header sticky
+    if (window.scrollY > 20) {
+        document.getElementById("header").classList.add("header-sticky");
+    } else {
+        document.getElementById("header").classList.remove("header-sticky");
+    }
+
     if (window.scrollY > 850) {
         document.getElementById("to-top").style.display = "flex";
     } else {
         document.getElementById("to-top").style.display = "none";
     }
+    
 })
