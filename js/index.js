b2beb3461d7a3dbd095aa94c12a30b65aa48d51e
Duc Tran <duc.tran@nfq.asia>
2022-05-19 18:02:12 +0700

    add: carousel section

--- js/index.js
+++ js/index.js
@@ -24,4 +24,54 @@ document.getElementById("searchInput").addEventListener("focusout", function() {
     document.querySelector(".header-search").style.borderColor = "#e2e2e2";
 });
 
+// carousel
+let slideIndex = 0;
+const carouselList = document.querySelectorAll(".carousel-item.card");
+const carouselLength = carouselList.length;
 
+function slideCarousel(number) {
+    if (slideIndex == carouselLength) {
+        slideIndex = 0;
+    } else if (slideIndex < 0) {
+        slideIndex = 3;
+    }
+    showCarousel(slideIndex += number)
+}
+
+function showCarousel(index) {
+    // remove previous active and display
+    for (elem of carouselList) {
+        elem.classList.remove("active")
+        elem.style.display = "none"
+        elem.style.order = "0"
+    }
+    
+
+    console.log(index)
+
+
+    // for active element
+    carouselList[index].classList.add("active")
+    carouselList[index].style.display = "flex";
+    carouselList[index].style.order = "2";
+
+
+    // for beside active element
+    if (index == 0) {
+        carouselList[carouselLength-1].style.display = "flex";
+        carouselList[carouselLength-1].style.order = "1";
+        carouselList[index+1].style.display = "flex";
+        carouselList[index+1].style.order = "3";
+    } else if (index == (carouselLength-1)) {
+        carouselList[0].style.display = "flex";
+        carouselList[0].style.order = "3";
+        carouselList[index-1].style.display = "flex";
+        carouselList[index-1].style.order = "1";
+    } else {
+        carouselList[index-1].style.display = "flex";
+        carouselList[index-1].style.order = "1";
+        carouselList[index+1].style.display = "flex";
+        carouselList[index+1].style.order = "3";
+    }
+};
+showCarousel(slideIndex);
